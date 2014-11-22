dbff99d6313402f0ea2c4b5c9b7aebc7bdb82f78
Cheuk Him Ho <fathimpeter@fathims-iMac.local>
2014-11-22 21:41:13 +0800

    Initial Commit

---
+++ BMITime/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BMITime
+//
+//  Created by Cheuk Him Ho on 22/11/14.
+//  Copyright (c) 2014 Cheuk Him Ho. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

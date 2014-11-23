5d02e8be3e940f22b142b6aab8518823961f6f56
Railgun3510 <Railgun3510@users.noreply.github.com>
2014-11-23 21:55:39 +0800

    Using dot notation

--- BMITime/main.m
+++ BMITime/main.m
@@ -14,18 +14,18 @@ int main(int argc, const char * argv[]) {
         // Create an instance of BNRPerson
         BNRPerson *mikey = [[BNRPerson alloc]init];
         
-        // Give the instance variables interesting values using setters
-        [mikey setWeightInKilos:96];
-        [mikey setHeightInMeters:1.8];
+        // Give the instance variables interesting values using setters using dot notation
+        mikey.weightInKilos = 96;
+        mikey.heightInMeters = 1.8;
         
         // Log the instance variables using the getters
-        float height = [mikey heightInMeters];
-        int weight = [mikey weightInKilos];
+        float height = mikey.heightInMeters;
+        int weight = mikey.weightInKilos;
         NSLog(@"mikey is %.2f meters tall and weights %d kilograms", height, weight);
         
         // Log some values using custom methods
-        float bmi = [mikey bodyMassIndex];
-        NSLog(@"mikey has a BMI of %.2f", bmi);
+        float bmi = mikey.bodyMassIndex;
+        NSLog(@"mikey has a BMI of %.1f", bmi);
     }
     return 0;
 }
